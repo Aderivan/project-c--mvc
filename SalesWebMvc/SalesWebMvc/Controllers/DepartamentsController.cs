4fb685ee79115fa970965735fdccabed17eb7b5b
Aderivan <aderivan.mkt@gmail.com>
2020-01-27 19:25:51 -0300

    Created Controller and View of departaments

---
+++ SalesWebMvc/SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> departaments = new List<Departament>();
+
+            departaments.Add(new Departament(1, "Eletronicos"));
+            departaments.Add(new Departament(2, "Fashion"));
+            return View(departaments);
+        }
+    }
+}
---
+++ SalesWebMvc/SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,18 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Departament()
+        {
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
