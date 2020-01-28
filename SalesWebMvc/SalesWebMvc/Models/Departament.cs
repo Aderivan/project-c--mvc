12f50d07ca23048cdaae7df0cb43772b6995bc76
Aderivan <aderivan.mkt@gmail.com>
2020-01-28 00:11:57 -0300

    Created model of Seller, SalesRecord and Departaments implementations's

--- SalesWebMvc/SalesWebMvc/Models/Departament.cs
+++ SalesWebMvc/SalesWebMvc/Models/Departament.cs
@@ -1,8 +1,36 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Departament
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Departament()
+        {
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller sr)
+        {
+            Sellers.Add(sr);
+        }
+
+        public double TotalSellers(DateTime dateInit, DateTime dateFinish)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(dateInit, dateFinish));
+        }
+
     }
+
+
 }
---
+++ SalesWebMvc/SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,30 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SalesStatus salesStatus { get; set; }
+        public Seller seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SalesStatus salesStatus, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            this.salesStatus = salesStatus;
+            this.seller = seller;
+        }
+    }
+}
